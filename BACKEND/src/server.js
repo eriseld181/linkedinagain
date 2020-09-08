a0cb43420711afdc8bc50799f14742b1f04a2ac7
segundara <segundara@gmail.com>
2020-09-08 08:18:10 +0300

    update to server js

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -11,7 +11,105 @@ const experienceRouter = require("./routes/experience")
 const post = require("./routes/post")
 const server = express()
 
+const {
+    addUserToRoom,
+    removeUser,
+    getUser,
+    getUsersInRoom,
+} = require("./chatUtils/users")
+const addMessage = require("./chatUtils/messages")
 
+const http = require("http")
+const socketio = require("socket.io")
+const httpServer = http.createServer(server)
+const io = socketio(httpServer)
+
+
+io.on("connection", (socket) => {
+    console.log("New WebSocket connection ", socket.id)
+
+    socket.on("join", async (options) => {
+        console.log("JOINED", options)
+        try {
+            const { username, room } = await addUserToRoom({
+                id: socket.id,
+                ...options,
+            })
+
+            socket.join(room)
+
+            const welcomeMessage = {
+                sender: "Admin",
+                text: `Welcome to ${room} channel`,
+                createdAt: new Date(),
+            }
+
+            socket.emit("message", welcomeMessage)
+
+            const messageToRoomMembers = {
+                sender: "Admin",
+                text: `${username} has joined!`,
+                createdAt: new Date(),
+            }
+
+            socket.broadcast.to(room).emit("message", messageToRoomMembers)
+
+            const roomMembers = await getUsersInRoom(room)
+            io.to(room).emit("roomData", {
+                room: room,
+                users: roomMembers,
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+    socket.on("sendMessage", async ({ text, room }) => {
+        console.log(text, room)
+        try {
+            const user = await getUser(room, socket.id)
+
+            const messageContent = {
+                sender: user.username,
+                text,
+                createdAt: new Date(),
+            }
+
+            const response = await addMessage(
+                messageContent.text,
+                user.username,
+                room
+            )
+            if (response) {
+                io.to(room).emit("message", messageContent)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+    socket.on("leaveRoom", async ({ room }) => {
+        try {
+            const user = await removeUser(socket.id, room)
+            const message = {
+                username: "Admin",
+                text: `${user.username} has left!`,
+                createdAt: new Date(),
+            }
+
+            const roomMembers = await getUsersInRoom(room)
+            if (user) {
+                io.to(room).emit("message", message)
+                io.to(room).emit("roomData", {
+                    room: room,
+                    users: roomMembers,
+                })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    })
+})
 
 // const {
 //     notFoundHandler,
@@ -49,7 +147,7 @@ mongoose.connect("mongodb+srv://eriseld:troy1894@cluster0.j7g0j.mongodb.net/link
     useUnifiedTopology: true,
 })
     .then(
-        server.listen(port || 3000, () => {
+        httpServer.listen(port || 3000, () => {
             console.log("Running on port", port || 3000)
         })
     )

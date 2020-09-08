bea397a58ec1476dbbf559e9e88c5c8fb4a2f826
MarigonaN <marigonanuhija@gmail.com>
2020-09-08 15:33:43 +0200

    commit

--- frontend-team/src/components/Login.jsx
+++ frontend-team/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, FormControl, Button, FormGroup, Image } from 'react-bootstrap'
+import { Container,  Button, Form, Image, Row, Col, Nav } from 'react-bootstrap'
 
 class Login extends Component {
 
@@ -23,7 +23,7 @@ class Login extends Component {
 
 
     fetchUser = async () => {
-        await fetch("https://striveschool.herokuapp.com/api/profile/", {
+        await fetch("http://localhost:4000/user/signup", {
             headers: new Headers({
                 'Authorization': 'Basic ' + "dXNlcjE2OmM5V0VVeE1TMjk0aE42ZkY=",
                 "Content-Type": "application/json",
@@ -35,69 +35,29 @@ class Login extends Component {
             }))
     }
 
-    componentDidMount = () => {
-        localStorage.removeItem("authorizationKey")
-        localStorage.removeItem("username")
-        this.fetchUser()
-        this.props.resetAuthorization()
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault()
-        let user = this.state.users.filter(user =>
-            user.name.toLowerCase() === this.state.name.toLowerCase() && user._id === this.state.password
-        )
-        if (user.length > 0 && user[0].name.toLowerCase() === "segun") {
-            this.props.getAuthorization("dXNlcjI0OjQ4RDR2YVZoNlJhM0REOHc=", "user24")
-            this.props.history.push("/profiles/" + user[0].username)
-            this.props.showApp()
-        } else if (user.length > 0 && user[0].name.toLowerCase() === "klevin") {
-            this.props.getAuthorization("dXNlcjE2OmM5V0VVeE1TMjk0aE42ZkY=", "user16")
-            this.props.history.push("/profiles/" + user[0].username)
-            this.props.showApp()
-        } else if (user.length > 0 && user[0].name.toLowerCase() === "nomfundo verah") {
-            this.props.getAuthorization("dXNlcjIxOjJydXhhNE1SSmRVZ2c2Y3o=", "user21")
-            this.props.history.push("/profiles/" + user[0].username)
-            this.props.showApp()
-        } else if (user.length > 0 && user[0].name.toLowerCase() === "oluwasijibomi hafeez") {
-            this.props.getAuthorization("dXNlcjE0Oko4M3M4SnN2eWRSTk5qTHQ=", "user14")
-            this.props.history.push("/profiles/" + user[0].username)
-            this.props.showApp()
-        }
-        else {
-            alert("Plase user just watch out what you type!")
-        }
-
-
-    }
+ 
 
     render() {
         return (
             <Container className="my-5">
             <Row>
               <Col md={3}></Col>
-              <Col md={6} className="mt-5" style={mystyle}>
+              <Col md={6} className="mt-5" >
                 <img
                   src="https://logos-world.net/wp-content/uploads/2020/04/Linkedin-Logo-2003%E2%80%932011.png"
                   style={{ width: "250px", marginLeft: "110px" }}
                 ></img>
                 <div className="mt-3 my-3 ml-3 mr-3">
-                  {!success && (
-                    <Alert variant={"danger"}>
-                      Incorrect username or password!
-                    </Alert>
-                  )}
+                  
                   <Form
-                    onSubmit={(e) => {
-                      login(e);
-                    }}
+                    
                   >
                     <Form.Group controlId="formBasicEmail">
                       <Form.Label>Username</Form.Label>
                       <Form.Control
                         type="text"
                         placeholder="Enter username"
-                        value={credentials.username}
+                       
                         onChange={(e) => setData({ username: e.target.value })}
                       />
                     </Form.Group>
@@ -107,7 +67,7 @@ class Login extends Component {
                       <Form.Control
                         type="password"
                         placeholder="Password"
-                        value={credentials.password}
+                     
                         onChange={(e) => setData({ password: e.target.value })}
                       />
                     </Form.Group>

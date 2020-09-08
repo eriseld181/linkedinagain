61145df1a6ba5e0b5a8b1e3d83aaa45081f6afe1
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2020-09-08 09:53:59 +0200

    updates

--- BACKEND/src/oauthAuthorization/oauth.js
+++ BACKEND/src/oauthAuthorization/oauth.js
@@ -2,5 +2,7 @@ const passport = require('passport')
 const GoogleStrategy = reuire("passport-google-oauth20").Strategy
 
 passport.use("google", new GoogleStrategy({
-    clientId: process.env.Google_Id
+    clientId: process.env.Google_Id,
+    clientSecret: process.env.Google_Secret,
+    callBackURL: " http://localhost:4000/users/google-redirect"
 }))

527bacbf0c7613b0dcb44e53e56c5e9bb143f65f
MarigonaN <marigonanuhija@gmail.com>
2020-09-11 11:54:02 +0200

    commit

--- frontend-team/src/components/Login.jsx
+++ frontend-team/src/components/Login.jsx
@@ -1,108 +1,397 @@
-// import React, { Component } from 'react';
-// import { Container, FormControl, Button, FormGroup, Image } from 'react-bootstrap'
+import React, { Component } from 'react';
+import {
+  Container,
+  FormControl,
+  Button,
+  FormGroup,
+  Image,
+  Alert,
+  Row,
+  Col,
+} from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 
-// class Login extends Component {
+import { FiUpload } from 'react-icons/fi';
 
-//     state = {
-//         name: '',
-//         password: '',
-//         users: []
-//     }
+const apiKey = process.env.REACT_APP_API;
 
-//     setName = (e) => {
-//         this.setState({
-//             name: e
-//         });
-//     }
+class Login extends Component {
+  state = {
+    photo: null,
+    user: {
+      credentials: '',
+      password: '',
+    },
+    signup: false,
+    login: true,
+    newUser: {
+      username: '',
+      email: '',
+      password: '',
+    },
+    profile: {
+      name: '',
+      surname: '',
+      email: '',
+      bio: '',
+      title: '',
+      about: '',
+      area: '',
+      image: '',
+      username: '',
+    },
+    token: '',
+    fromLogin: false,
+  };
 
-//     setPassword = (e) => {
-//         this.setState({
-//             password: e
-//         });
-//     }
+  setName = (e) => {
+    const user = this.state.user;
+    user.credentials = e;
+    this.setState({
+      user,
+    });
+  };
 
+  handleUpload = async () => {
+    const photo = new FormData();
+    photo.append('profile', this.state.photo);
 
-//     fetchUser = async () => {
-//         await fetch("https://striveschool.herokuapp.com/api/profile/", {
-//             headers: new Headers({
-//                 'Authorization': 'Basic ' + "dXNlcjE2OmM5V0VVeE1TMjk0aE42ZkY=",
-//                 "Content-Type": "application/json",
-//             }),
-//         })
-//             .then(resp => resp.json())
-//             .then(respObj => this.setState({
-//                 users: respObj
-//             }))
-//     }
+    await fetch(
+      apiKey + '/api/profile/' + this.state.profile.username + '/upload',
+      {
+        method: 'POST',
+        body: photo,
+        headers: new Headers({
+          Authorization: 'Bearer ' + this.state.token,
+        }),
+      }
+    );
+  };
 
-//     componentDidMount = () => {
-//         localStorage.removeItem("authorizationKey")
-//         localStorage.removeItem("username")
-//         this.fetchUser()
-//         this.props.resetAuthorization()
-//     }
+  setPassword = (e) => {
+    const user = this.state.user;
+    user.password = e;
+    this.setState({
+      user,
+    });
+  };
 
-//     handleSubmit = (e) => {
-//         e.preventDefault()
-//         let user = this.state.users.filter(user =>
-//             user.name.toLowerCase() === this.state.name.toLowerCase() && user._id === this.state.password
-//         )
-//         if (user.length > 0 && user[0].name.toLowerCase() === "segun") {
-//             this.props.getAuthorization("dXNlcjI0OjQ4RDR2YVZoNlJhM0REOHc=", "user24")
-//             this.props.history.push("/profiles/" + user[0].username)
-//             this.props.showApp()
-//         } else if (user.length > 0 && user[0].name.toLowerCase() === "klevin") {
-//             this.props.getAuthorization("dXNlcjE2OmM5V0VVeE1TMjk0aE42ZkY=", "user16")
-//             this.props.history.push("/profiles/" + user[0].username)
-//             this.props.showApp()
-//         } else if (user.length > 0 && user[0].name.toLowerCase() === "nomfundo verah") {
-//             this.props.getAuthorization("dXNlcjIxOjJydXhhNE1SSmRVZ2c2Y3o=", "user21")
-//             this.props.history.push("/profiles/" + user[0].username)
-//             this.props.showApp()
-//         } else if (user.length > 0 && user[0].name.toLowerCase() === "oluwasijibomi hafeez") {
-//             this.props.getAuthorization("dXNlcjE0Oko4M3M4SnN2eWRSTk5qTHQ=", "user14")
-//             this.props.history.push("/profiles/" + user[0].username)
-//             this.props.showApp()
-//         }
-//         else {
-//             alert("Plase user just watch out what you type!")
-//         }
+  handleChange = (e) => {
+    const newUser = this.state.newUser;
+    newUser[e.currentTarget.id] = e.currentTarget.value;
+    this.setState({
+      newUser,
+    });
+  };
 
+  handleChangeProfile = (e) => {
+    const profile = this.state.profile;
+    profile[e.currentTarget.id] = e.currentTarget.value;
+    this.setState({
+      profile,
+    });
+  };
 
-//     }
+  fetchUser = async () => {
+    let resp = await fetch(apiKey + '/api/profile/login', {
+      method: 'POST',
+      body: JSON.stringify(this.state.user),
+      credentials: 'include',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+    });
+    if (resp.ok) {
+      this.props.getAuthorization(this.state.user.credentials);
 
-//     render() {
-//         return (
-//             <Container id="logingPage" className="d-flex justify-content-center ">
+      this.props.history.push('/profiles/me?' + this.state.user.credentials);
+    } else {
+      this.setState({ signup: true, fromLogin: true });
+    }
+  };
 
-//                 <div className="Login">
-//                     <Image className="mb-3" src="https://cdn.worldvectorlogo.com/logos/linkedin.svg" />
-//                     <form onSubmit={this.handleSubmit}>
-//                         <FormGroup controlId="name" bsSize="large">
-//                             <label>Email</label>
-//                             <FormControl
-//                                 autoFocus
-//                                 type="name"
-//                                 value={this.state.name}
-//                                 onChange={e => this.setName(e.target.value)}
-//                             />
-//                         </FormGroup>
-//                         <FormGroup controlId="password" bsSize="large">
-//                             <label>Password</label>
-//                             <FormControl
-//                                 value={this.state.password}
-//                                 onChange={e => this.setPassword(e.target.value)}
-//                                 type="password"
-//                             />
-//                         </FormGroup>
-//                         <Button block bsSize="large" type="submit">
-//                             Login
-//                             </Button>
-//                     </form>
-//                 </div>
-//             </Container>
-//         );
-//     }
-// }
+  componentDidMount() {
+    this.props.resetAuthorization();
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.fetchUser();
+  };
 
-// export default Login;
+  addPhoto = (e) => {
+    const photo = e.target.files[0];
+
+    this.setState({
+      photo,
+    });
+  };
+
+  addNewProfile = async (e) => {
+    e.preventDefault();
+    const resp = await fetch(apiKey + '/api/profile', {
+      method: 'POST',
+      body: JSON.stringify({
+        ...this.state.profile,
+        ...this.state.newUser,
+      }),
+      credentials: 'include',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+    });
+    const photo = new FormData();
+    photo.append('profile', this.state.photo);
+    const id = await resp.json();
+
+    const resp2 = await fetch(apiKey + '/api/profile/' + id + '/upload', {
+      method: 'POST',
+      body: photo,
+      credentials: 'include',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+
+    this.setState({ signup: false, login: true });
+  };
+
+  render() {
+    return (
+      <Container id='loginPage' className='d-flex justify-content-center '>
+        {this.state.login ? (
+          <div className='Login'>
+            <Image
+              className='mb-3'
+              src='https://cdn.worldvectorlogo.com/logos/linkedin.svg'
+            />
+            <form onSubmit={this.handleSubmit}>
+              <FormGroup controlId='name'>
+                <label>Email</label>
+                <FormControl
+                  autoFocus
+                  type='name'
+                  value={this.state.name}
+                  onChange={(e) => this.setName(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup controlId='password'>
+                <label>Password</label>
+                <FormControl
+                  value={this.state.password}
+                  onChange={(e) => this.setPassword(e.target.value)}
+                  type='password'
+                />
+              </FormGroup>
+              <Button block type='submit'>
+                Login
+              </Button>
+              <div className='logins'>
+                <a
+                  href={
+                    process.env.REACT_APP_API + '/api/profile/auth/facebook'
+                  }
+                >
+                  <Image src='/assets/Sign-In-Large_FB.png' />
+                </a>
+                <a
+                  href={
+                    process.env.REACT_APP_API + '/api/profile/auth/linkedin'
+                  }
+                >
+                  <Image src='/assets/Sign-In-Large.png' />
+                </a>
+              </div>
+              <br />
+
+              <div className='d-flex justify-content-center mt-2'>
+                <Button
+                  id='createAccLink'
+                  onClick={() =>
+                    this.setState({
+                      signup: !this.state.signup,
+                      fromLogin: false,
+                    })
+                  }
+                >
+                  Create an account
+                </Button>
+              </div>
+              {this.state.signup ? (
+                <>
+                  <div className='mt-5'>
+                    {this.state.fromLogin ? (
+                      <Alert variant='warning'>
+                        "We cannot find users with this credentials"
+                      </Alert>
+                    ) : (
+                      <div className='d-flex justify-content-center'>
+                        <Button
+                          variant='info'
+                          onClick={() =>
+                            this.setState({ login: false, signup: true })
+                          }
+                        >
+                          Create a new User
+                        </Button>
+                      </div>
+                    )}
+                  </div>
+                </>
+              ) : null}
+            </form>
+          </div>
+        ) : (
+          <>
+            {this.state.signup ? (
+              <div className='Login'>
+                <Image
+                  className='mb-3'
+                  src='https://cdn.worldvectorlogo.com/logos/linkedin.svg'
+                />
+                <form>
+                  <FormGroup>
+                    <label>Username</label>
+                    <FormControl
+                      autoFocus
+                      id='username'
+                      type='text'
+                      value={this.state.newUser.username}
+                      onChange={(e) => this.handleChange(e)}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <label>Email</label>
+                    <FormControl
+                      value={this.state.newUser.email}
+                      onChange={(e) => this.handleChange(e)}
+                      type='text'
+                      id='email'
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <label>Password</label>
+                    <FormControl
+                      value={this.state.newUser.password}
+                      onChange={(e) => this.handleChange(e)}
+                      type='password'
+                      id='password'
+                    />
+                  </FormGroup>
+                  <Button
+                    block
+                    onClick={() => this.setState({ signup: false })}
+                  >
+                    Sign Up
+                  </Button>
+                </form>
+              </div>
+            ) : (
+              <div style={{ marginTop: '35vh' }}>
+                <Container className='d-flex justify-content-center'>
+                  <Row>
+                    <Col className='d-flex align-items-center mr-5'>
+                      <label htmlFor='profilePhoto'>
+                        <FiUpload
+                          style={{ fontSize: '55px', color: '#0073B1' }}
+                        />
+                      </label>
+                    </Col>
+                    <form onSubmit={this.addNewProfile}>
+                      <input
+                        style={{ display: 'none' }}
+                        type='file'
+                        id='profilePhoto'
+                        profile='file'
+                        onChange={(e) => this.addPhoto(e)}
+                        accept='image/*'
+                      />
+                      <Container>
+                        <Image
+                          src='https://cdn.worldvectorlogo.com/logos/linkedin.svg'
+                          className='mb-3'
+                          style={{ width: '150px' }}
+                        />
+                        <Row>
+                          <Col>
+                            <FormGroup>
+                              <label>Name</label>
+                              <FormControl
+                                autoFocus
+                                id='name'
+                                type='text'
+                                value={this.state.profile.name}
+                                onChange={(e) => this.handleChangeProfile(e)}
+                              />
+                            </FormGroup>
+                            <FormGroup>
+                              <label>Surname</label>
+                              <FormControl
+                                autoFocus
+                                id='surname'
+                                type='text'
+                                value={this.state.profile.surname}
+                                onChange={(e) => this.handleChangeProfile(e)}
+                              />
+                            </FormGroup>
+                            <FormGroup>
+                              <label>About You</label>
+                              <FormControl
+                                autoFocus
+                                id='about'
+                                type='text'
+                                value={this.state.profile.about}
+                                onChange={(e) => this.handleChangeProfile(e)}
+                              />
+                            </FormGroup>
+                          </Col>
+                          <Col>
+                            <FormGroup>
+                              <label>Bio</label>
+                              <FormControl
+                                autoFocus
+                                id='bio'
+                                type='text'
+                                value={this.state.profile.bio}
+                                onChange={(e) => this.handleChangeProfile(e)}
+                              />
+                            </FormGroup>
+                            <FormGroup>
+                              <label>Title</label>
+                              <FormControl
+                                autoFocus
+                                id='title'
+                                type='text'
+                                value={this.state.profile.title}
+                                onChange={(e) => this.handleChangeProfile(e)}
+                              />
+                            </FormGroup>
+                            <FormGroup>
+                              <label>Area</label>
+                              <FormControl
+                                autoFocus
+                                id='area'
+                                type='text'
+                                value={this.state.profile.area}
+                                onChange={(e) => this.handleChangeProfile(e)}
+                              />
+                            </FormGroup>
+                          </Col>
+                        </Row>
+                        <Button block type='submit'>
+                          Add Info
+                        </Button>
+                      </Container>
+                    </form>
+                  </Row>
+                </Container>
+              </div>
+            )}
+          </>
+        )}
+      </Container>
+    );
+  }
+}
+
+export default Login;
